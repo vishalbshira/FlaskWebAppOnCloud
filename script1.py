9cb1cf5b448b47f1d30dc5530055c2de38d47341
Vishal <vishalbshira@gmail.com>
2020-10-02 17:29:50 +0200

    added code files

---
+++ script1.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='Templates')
+
+#route path
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
